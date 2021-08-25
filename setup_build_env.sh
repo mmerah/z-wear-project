1d591724e371699c3a5891e5d2fdb7af20f9f723
mmerah <mohammedimerah@nanoleq.com>
2021-08-25 14:55:51 +0200

    Manually set up nrf-command-tools instead of using conda bc it does not seem to work

--- setup_build_env.sh
+++ setup_build_env.sh
@@ -7,6 +7,22 @@
 sudo apt install gcc
 sudo apt install gcc-multilib
 
+# Install nRF Command Line Tools. Version 10.13.0
+wget https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-10-x-x/10-13-0/nRF-Command-Line-Tools_10_13_0_Linux64.zip
+sudo apt-get install unzip
+unzip nRF-Command-Line-Tools_10_13_0_Linux64.zip
+tar -xf nRF-Command-Line-Tools_10_13_0_Linux64/nRF-Command-Line-Tools_10_13_0_Linux-amd64.tar.gz
+chmod +x JLink_Linux_V750a_x86_64.deb
+chmod +x nRF-Command-Line-Tools_10_13_0_Linux-amd64.deb
+sudo dpkg -i JLink_Linux_V750a_x86_64.deb
+sudo dpkg -i nRF-Command-Line-Tools_10_13_0_Linux-amd64.deb
+rm -R nRF-Command-Line-Tools_10_13_0_Linux64
+rm JLink_Linux_V750a_x86_64.*
+rm nRF-Command-Line-Tools_10_13_0.*
+rm nRF-Command-Line-Tools_10_13_0_Linux64.*
+rm nRF-Command-Line-Tools_10_13_0_Linux-amd64.deb
+rm README.txt
+
 # Setup the developer environment (mainly for west). Versions in the yml file
 conda env create -n dev_env -f z-wear_env.yml
 conda activate dev_env
